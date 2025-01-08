16fc372173f2256e37131b1065880b847ed42ac6
masaki <masaki.git@mailc.net>
2025-01-09 01:51:29 +0900

    Update transfer script to send all remaining balance

--- transfer.ts
+++ transfer.ts
@@ -10,15 +10,35 @@ const connection = new Connection("https://api.devnet.solana.com");
 
 (async () => {
   try {
+    // Get balance of dev wallet
+    const balance = await connection.getBalance(from.publicKey)
+
+    // Create a test transaction to calculate fees
     const transaction = new Transaction().add(
       SystemProgram.transfer({
         fromPubkey: from.publicKey,
         toPubkey: to,
-        lamports: LAMPORTS_PER_SOL / 100,
+        lamports: balance,
       })
     );
     transaction.recentBlockhash = (await connection.getLatestBlockhash('confirmed')).blockhash;
     transaction.feePayer = from.publicKey;
+
+    // Calculate exact fee rate to transfer entire SOL amount out of account minus fees
+    const fee = (await connection.getFeeForMessage(transaction.compileMessage(), 'confirmed')).value || 0;
+
+    // Remove our transfer instruction to replace it
+    transaction.instructions.pop();
+
+    // Now add the instruction back with correct amount of lamports
+    transaction.add(
+      SystemProgram.transfer({
+        fromPubkey: from.publicKey,
+        toPubkey: to,
+        lamports: balance - fee,
+      })
+    );
+
     // Sign transaction, broadcast, and confirm
     const signature = await sendAndConfirmTransaction(
       connection,
@@ -26,7 +46,7 @@ const connection = new Connection("https://api.devnet.solana.com");
       [from]
     );
     console.log(`Success! Check out your TX here:
-https://explorer.solana.com/tx/${signature}?cluster=devnet`);
+https://explorer.solana.com/tx/${signature}?cluster=devnet`)
   } catch (e) {
     console.error(`Oops, something went wrong: ${e}`)
   }
